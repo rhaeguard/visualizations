f9d11225ebf7d45ca85f3e01be25702188ba0b6f
Mansur <mensurowary@gmail.com>
2023-04-04 01:48:50 -0600

    ascii: minor updates + references

---
+++ ascii_image/README.md
@@ -0,0 +1,5 @@
+# References
+
+- https://www.youtube.com/watch?v=55iwMYv8tGI
+- http://paulbourke.net/dataformats/asciiart/
+- https://web.dev/getusermedia-intro/
--- ascii_image/main.js
+++ ascii_image/main.js
@@ -1,6 +1,7 @@
 const WIDTH = 640;
 const HEIGHT = 480;
 
+// from blackest to whitest
 const DENSITY = `$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^\`'. `;
 
 const canvas = document.getElementById('canvas');
@@ -16,9 +17,9 @@ function drawAscii(w, h) {
         for (let x = 0; x < w; x += 3) {
             const pixel = ctx.getImageData(x, y, 1, 1);
             const [r, g, b, a] = pixel.data;
-            const grey = Math.ceil((r + g + b) / 3);
+            const grey = Math.floor((r + g + b) / 3);
 
-            const pos = Math.floor(grey / 3.8);
+            const pos = Math.floor(grey / 3.8); // 255 / 69 ~~ 3.8
             const ch = DENSITY.charAt(pos);
             line += ch;
         }
@@ -26,18 +27,17 @@ function drawAscii(w, h) {
     }
 }
 
-function cameraStuff() {
+function captureCameraInputAndDraw() {
     const player = document.getElementById('player');
     player.width = WIDTH;
     player.height = HEIGHT;
     player.style.display = "none";
 
-
     const constraints = {
         video: true,
     };
 
-    const intervalHandle = setInterval(() => {
+    setInterval(() => {
         ctx.drawImage(player, 0, 0, WIDTH / 1.5, HEIGHT / 1.5);
         drawAscii(WIDTH / 1.5, HEIGHT / 1.5);
     }, 100);
@@ -48,4 +48,4 @@ function cameraStuff() {
     });
 }
 
-cameraStuff();
+captureCameraInputAndDraw();
