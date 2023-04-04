da3c161bded2d3a73155270d965ef170dde16a18
Mansur <mensurowary@gmail.com>
2023-04-04 01:33:47 -0600

    ascii: getting a continuous camera input

--- ascii_image/README.md
+++
@@ -1,3 +0,0 @@
-# Credit
-
-Photo by <a href="https://unsplash.com/it/@jakenackos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jake Nackos</a> on <a href="https://unsplash.com/photos/IF9TK5Uy-KI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- ascii_image/main.js
+++ ascii_image/main.js
@@ -5,43 +5,47 @@ const DENSITY = `$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:
 
 const canvas = document.getElementById('canvas');
 canvas.style.display = 'none';
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 const ctx = canvas.getContext('2d');
 
-function onload() {
-    const imgUrl = "./image.jpg";
+function drawAscii(w, h) {
+    document.getElementById("result").innerText = "";
+    for (let y = 0; y < h; y += 6) {
+        let line = "";
+        for (let x = 0; x < w; x += 3) {
+            const pixel = ctx.getImageData(x, y, 1, 1);
+            const [r, g, b, a] = pixel.data;
+            const grey = Math.ceil((r + g + b) / 3);
 
-    const img = new Image();
-    img.crossOrigin = 'anonymous';
-    img.src = imgUrl;
-    img.onload = function () {
-        // Use the intrinsic size of image in CSS pixels for the canvas element
-        const w = Math.round(this.naturalWidth / 3);
-        const h = Math.round(this.naturalHeight / 3);
-
-        canvas.width = w;
-        canvas.height = h;
-
-        // To use the custom size we'll have to specify the scale parameters
-        // using the element's width and height properties - lets draw one
-        // on top in the corner:
-        ctx.drawImage(this, 0, 0, w, h);
-        this.style.display = 'none';
-
-        const start = performance.now();
-        for (let y = 0; y < h; y += 6) {
-            let line = "";
-            for (let x = 0; x < w; x += 3) {
-                const pixel = ctx.getImageData(x, y, 1, 1);
-                const [r, g, b, a] = pixel.data;
-                const grey = Math.ceil((r + g + b) / 3);
-
-                const pos = Math.floor(grey / 3.8);
-                const ch = DENSITY.charAt(pos);
-                line += ch;
-            }
-            document.getElementById("result").innerText += line + "\n";
+            const pos = Math.floor(grey / 3.8);
+            const ch = DENSITY.charAt(pos);
+            line += ch;
         }
+        document.getElementById("result").innerText += line + "\n";
     }
 }
 
-onload();
+function cameraStuff() {
+    const player = document.getElementById('player');
+    player.width = WIDTH;
+    player.height = HEIGHT;
+    player.style.display = "none";
+
+
+    const constraints = {
+        video: true,
+    };
+
+    const intervalHandle = setInterval(() => {
+        ctx.drawImage(player, 0, 0, WIDTH / 1.5, HEIGHT / 1.5);
+        drawAscii(WIDTH / 1.5, HEIGHT / 1.5);
+    }, 100);
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        // Attach the video stream to the video element and autoplay.
+        player.srcObject = stream;
+    });
+}
+
+cameraStuff();
